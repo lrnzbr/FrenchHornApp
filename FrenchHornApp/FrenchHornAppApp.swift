a327975404e1f664f90c6e7cb51f635f1e15c1bc
Lorenzo Brown <lrnzbr@gmail.com>
2022-08-29 15:38:53 -0500

    Initial Commit

---
+++ FrenchHornApp/FrenchHornAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrenchHornAppApp.swift
+//  FrenchHornApp
+//
+//  Created by Lorenzo Brown on 8/29/22.
+//
+
+import SwiftUI
+
+@main
+struct FrenchHornAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
