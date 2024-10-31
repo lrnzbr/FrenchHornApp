c556d9c454fb44ab34c48a20b7f7cf68f4f9c954
Lorenzo Brown <lrnzbr@gmail.com>
2024-10-30 23:24:54 -0600

    Version bug fix

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo ">>> SETING UP ENVIRONMENT"
+
+
+echo ">>> UPDATING BUILD NUMBER: $CI_BUILD_NUMBER"
+
+plutil -replace CFBundleVersion -string "$CI_BUILD_NUMBER" $CI_PRIMARY_REPOSITORY_PATH/TechApp/Resources/Info.plist
+plutil -replace CFBundleShortVersionString -string "1.2.${CI_BUILD_NUMBER}" $CI_PRIMARY_REPOSITORY_PATH/TechApp/Resources/Info.plist
