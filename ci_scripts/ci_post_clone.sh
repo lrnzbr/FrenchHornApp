a7c609cd9e9070dc15092597e58da932a34d0016
Lorenzo Brown <lrnzbr@gmail.com>
2024-10-30 23:32:48 -0600

    plist fix

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -4,5 +4,5 @@ echo ">>> SETING UP ENVIRONMENT"
 
 echo ">>> UPDATING BUILD NUMBER: $CI_BUILD_NUMBER"
 
-plutil -replace CFBundleVersion -string "$CI_BUILD_NUMBER" $CI_PRIMARY_REPOSITORY_PATH/TechApp/Resources/Info.plist
-plutil -replace CFBundleShortVersionString -string "1.2.${CI_BUILD_NUMBER}" $CI_PRIMARY_REPOSITORY_PATH/TechApp/Resources/Info.plist
+plutil -replace CFBundleVersion -string "$CI_BUILD_NUMBER" $CI_PRIMARY_REPOSITORY_PATH/FrenchHornApp/Info.plist
+plutil -replace CFBundleShortVersionString -string "1.2.${CI_BUILD_NUMBER}" $CI_PRIMARY_REPOSITORY_PATH/FrenchHornApp/Info.plist
