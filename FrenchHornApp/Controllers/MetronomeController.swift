f4a13dce3f0344fce3a7c5231bfe62a460cbf29c
Lorenzo Brown <lrnzbr@gmail.com>
2026-04-20 16:58:18 -0600

    metronome feature

--- FrenchHornApp/Controllers/MetronomeController.swift
+++ FrenchHornApp/Controllers/MetronomeController.swift
@@ -20,12 +20,24 @@ class MetronomeController: ObservableObject {
         if let regularPath = Bundle.main.path(forResource: "regularBeat", ofType: "mp3") {
             regularBeatSound = try? AVAudioPlayer(contentsOf: URL(fileURLWithPath: regularPath))
             regularBeatSound?.prepareToPlay()
+        } else if let data = Self.syntheticClickWAV(accent: false) {
+            regularBeatSound = try? AVAudioPlayer(data: data, fileTypeHint: AVFileType.wav.rawValue)
+            regularBeatSound?.prepareToPlay()
         }
         
         if let accentedPath = Bundle.main.path(forResource: "accentedBeat", ofType: "mp3") {
             accentedBeatSound = try? AVAudioPlayer(contentsOf: URL(fileURLWithPath: accentedPath))
             accentedBeatSound?.prepareToPlay()
+        } else if let data = Self.syntheticClickWAV(accent: true) {
+            accentedBeatSound = try? AVAudioPlayer(data: data, fileTypeHint: AVFileType.wav.rawValue)
+            accentedBeatSound?.prepareToPlay()
         }
+        
+        #if DEBUG
+        if regularBeatSound == nil || accentedBeatSound == nil {
+            print("MetronomeController: failed to load beat sounds (bundle MP3s missing and synthetic fallback failed).")
+        }
+        #endif
     }
     
     func start() {
@@ -33,9 +45,13 @@ class MetronomeController: ObservableObject {
         currentBeat = 0
         
         let timeInterval = 60.0 / bpm
-        timer = Timer.scheduledTimer(withTimeInterval: timeInterval, repeats: true) { [weak self] _ in
+        playBeat()
+        
+        let t = Timer(timeInterval: timeInterval, repeats: true) { [weak self] _ in
             self?.playBeat()
         }
+        RunLoop.main.add(t, forMode: .common)
+        timer = t
     }
     
     func stop() {
@@ -55,4 +71,56 @@ class MetronomeController: ObservableObject {
         
         currentBeat = (currentBeat + 1) % beatsInMeasure
     }
-} 
+    
+    /// Short sine burst as WAV data when `regularBeat.mp3` / `accentedBeat.mp3` are not in the app bundle.
+    private static func syntheticClickWAV(accent: Bool) -> Data? {
+        let sampleRate = 44_100.0
+        let frequency = accent ? 1_200.0 : 880.0
+        let duration = accent ? 0.055 : 0.038
+        let sampleCount = Int(sampleRate * duration)
+        guard sampleCount > 0 else { return nil }
+        
+        var samples = [Int16](repeating: 0, count: sampleCount)
+        for i in 0..<sampleCount {
+            let t = Double(i) / sampleRate
+            let envelope = exp(-t * (accent ? 48.0 : 62.0))
+            let s = sin(2.0 * Double.pi * frequency * t) * envelope * 0.88
+            samples[i] = Int16(max(-32_767, min(32_767, s * 32_767.0)))
+        }
+        return pcmMono16WAVData(samples: samples, sampleRate: UInt32(sampleRate))
+    }
+    
+    private static func pcmMono16WAVData(samples: [Int16], sampleRate: UInt32) -> Data? {
+        let numChannels: UInt16 = 1
+        let bitsPerSample: UInt16 = 16
+        let byteRate = sampleRate * UInt32(numChannels) * UInt32(bitsPerSample / 8)
+        let blockAlign = numChannels * (bitsPerSample / 8)
+        let dataSize = UInt32(samples.count * MemoryLayout<Int16>.size)
+        let riffChunkSize = 36 + dataSize
+        
+        var data = Data()
+        data.append(contentsOf: "RIFF".utf8)
+        data.append(contentsOf: riffChunkSize.leBytes)
+        data.append(contentsOf: "WAVE".utf8)
+        data.append(contentsOf: "fmt ".utf8)
+        data.append(contentsOf: UInt32(16).leBytes)
+        data.append(contentsOf: UInt16(1).leBytes)
+        data.append(contentsOf: numChannels.leBytes)
+        data.append(contentsOf: sampleRate.leBytes)
+        data.append(contentsOf: byteRate.leBytes)
+        data.append(contentsOf: blockAlign.leBytes)
+        data.append(contentsOf: bitsPerSample.leBytes)
+        data.append(contentsOf: "data".utf8)
+        data.append(contentsOf: dataSize.leBytes)
+        for s in samples {
+            data.append(contentsOf: s.littleEndian.leBytes)
+        }
+        return data
+    }
+}
+
+private extension FixedWidthInteger {
+    var leBytes: [UInt8] {
+        withUnsafeBytes(of: littleEndian) { Array($0) }
+    }
+}
